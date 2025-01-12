5d20328a1a226e3917f76bdfc0a056a61a13aa49
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-01-11 21:38:23 -0700

    Time: 1068 ms (27.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,4 @@
+select s.user_id, ROUND(AVG(case when c.action = 'confirmed' then 1.0 else 0.0 end), 2) as confirmation_rate
+from signups s left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
