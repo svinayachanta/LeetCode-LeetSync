79038628102b890bbb83adae3cd93257c8346dee
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-01-12 04:50:24 -0700

    Time: 1544 ms (21.66%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,4 +1,8 @@
-select class
+with cte as (
+select class, count(distinct student) as cnt
 from courses
-group by class
-having count(class) >= 5
+group by class)
+
+select class
+from cte 
+where cnt >= 5
