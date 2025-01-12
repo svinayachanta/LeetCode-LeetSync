7868532e78d19b38df488692540dcf294cae99db
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-01-12 05:05:12 -0700

    Time: 694 ms (25.55%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,9 @@
-select max(num) as num
-from (
-    select num
-    from MyNumbers
+with cte as (
+    select num, count(num) as cnt
+    from mynumbers
     group by num
-    having count(num) < 2
-) as sq
+    having count(num) = 1
+)
+
+select max(num) as num
+from cte
