06cbb67220a6be859c6e9e336a3804b729266431
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-01-13 12:31:26 -0700

    Time: 1169 ms (18.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,18 @@
+SELECT * FROM (
+    SELECT TOP 1 u.name AS results
+    FROM movierating mr
+    LEFT JOIN users u ON mr.user_id = u.user_id
+    GROUP BY u.name
+    ORDER BY COUNT(mr.movie_id) DESC, u.name
+) a
+
+UNION ALL
+
+SELECT * FROM (
+    SELECT TOP 1 m.title AS results
+    FROM movierating mr
+    LEFT JOIN movies m ON mr.movie_id = m.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.title
+    ORDER BY AVG(CAST(mr.rating AS FLOAT)) DESC, m.title
+) b;
