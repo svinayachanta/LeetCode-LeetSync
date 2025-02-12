d15d598e602755c972118349f7178d978cb41edf
Satyanarayana Vinay Achanta <160984161+svinayachanta@users.noreply.github.com>
2025-02-12 13:54:28 -0700

    Time: 277 ms (95.44%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,6 +1,9 @@
-select x, y, z, 
-case 
-when x+y>z and x+z>y and y+z>x then 'Yes'
+select
+x,
+y,
+z,
+case
+when x + y > z and x + z > y and y + z > x then 'Yes'
 else 'No'
-end as Triangle
+end as triangle
 from triangle
